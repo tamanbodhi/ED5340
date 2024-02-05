ced34ebc2ff146082b9808a53d9a2dabbb139104
bincy antony m <bincyndl@gmail.com>
2024-02-06 00:58:58 +0530

    ed5340 cw2

---
+++ CW2.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# Class 2 on 19012024
+
+# 10//-3
+
+# In[1]:
+
+
+10//-3
+
+
+# In[2]:
+
+
+10%-3
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#some questions to explore
+Find out the regular math function
+Find out trig fun
+find out rounding fun
+
+
+# In python there is no unary operator
+
+# Given x and y , find quotient and reminder. Round them to two digits after decimal
+# 
+
+# In[4]:
+
+
+x=10
+y=4
+x=input("first no")
+y=input("second no")
+c=round(x//y,2)
+d=round(x%y,2)
+print(c)
+print(d)
+
+
+# In[6]:
+
+
+##Everything in python is an object##
+
+
+# In[10]:
+
+
+a=2+3j
+print(a)
+print(a.real)
+print(a.imag)
+print(a.conjugate())
+
+
+# In[ ]:
+
+
+# Built in function(we can directly use, like conjugate)
+# memeber function
+# Library function(eg cos function, we have to import)
+
+
+# In[11]:
+
+
+import math
+pi=math.pi
+r=5
+area=pi*r**2
+print(area)
+area1=math.pi*r**2
+print(area1)
+
+
+
+# In[12]:
+
+
+Find math, round, trig, commenting,multiline comment
+
+
+# In[18]:
+
+
+#Strings
+a="Ram"
+b='Ram'
+c='''Ram'''
+d="""Ram"""
+print(a,b,c,d)
+msg1="I 'm learning python  now"
+print(msg1)
+#msg2='I 'm learning python  now'
+#print(msg2)
+rawstr1=r"I m leaving"
+print(rawstr)
+rawstr2=r'"Ram'# raw string ---prints as such
+print(rawstr2)
+
+print(multi1)
+
+
+# In[19]:
+
+
+# Accessing Array Elements
+Str1="Raman"
+print(Str1[0])
+
+
+# In[20]:
+
+
+print(Str1[-1])
+
+
+# In[21]:
+
+
+print(Str1[0:5])
+
+
+# In[22]:
+
+
+print(Str1[0:])
+
+
+# In[28]:
+
+
+print(Str1[:4])
+print(id(Str1))
+
+
+# print(Str1[-0:])
+
+# In[25]:
+
+
+# String is immutable
+Str1[0]=T
+
+
+# In[26]:
+
+
+Str1="Taman"
+
+
+# In[29]:
+
+
+print(Str1)
+print(id(Str1))
+
+
+# In[ ]:
+
+
+
+
